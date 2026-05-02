482f95ed7e92711f37b99f8d688d0249b2a2f252
Wahhaj Farooq <wahhaj@Wahhajs-MacBook-Pro.local>
2026-05-02 15:28:09 +1000

    Fixed base path to match repo URL

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/photoelectric-effect-simulation/',
+  base: '/Photoelectric-effect-simulation/',
 })
