fe32c0c09b7e65221d6040b3003978d64a009f2a
Wahhaj Farooq <wahhaj@Wahhajs-MacBook-Pro.local>
2026-05-02 15:31:09 +1000

    fixed base URL

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Photoelectric-effect-simulation/',
+  base: '/photoelectric-effect-simulation/',
 })
